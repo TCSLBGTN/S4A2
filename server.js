11929dcbfb8973f9508b8acd5163767b99cc6d4a
techninjas4 <44825619+techninjas4@users.noreply.github.com>
2018-11-11 23:40:49 +0000

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var io = require('socket.io').listen(server);  //pass a http.Server instance
 
 
 //connect to MongoDB
-mongoose.connect('mongodb://35.176.153.178:27017/ForAuth',{ useNewUrlParser: true });
+mongoose.connect('mongodb://3.8.123.191:27017/ForAuth',{ useNewUrlParser: true });
 var db = mongoose.connection;
 
 //handle mongo error
